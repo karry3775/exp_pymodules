0279f2c438db098c52b148918188eedde7cec9d9
karry3775 <kartikprakash3775@gmail.com>
2021-09-11 16:01:28 -0400

    Add config files

---
+++ grep_func/tests/test_grep_func.py
@@ -0,0 +1,21 @@
+import pytest
+from grep_func import grep_func
+from io import BytesIO
+import sys
+import re
+from colorama import Fore
+
+def test_grep():
+    import logging
+    pattern = "basic"
+    
+    sys.stdout = BytesIO()
+    
+    grep_func.grep_func(logging, pattern)
+
+    out_val = str(sys.stdout.getvalue())
+
+    sys.stdout = sys.__stdout__ # restores original stdout
+
+    val = re.sub(r'({})'.format(pattern), Fore.RED + r'\1' + Fore.RESET, "basicConfig") + "\n"
+    assert out_val == val
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="grep_func",
+    version="2.0.0",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
